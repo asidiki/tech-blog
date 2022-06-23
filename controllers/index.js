db95de8fcab711659fe325c900bba0cd6baf5cc2
Ansab Sidiki <ansab@sidiki.io>
2022-06-23 18:06:38 -0400

    home routes created

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
+// const dashboardRoutes = require("./dashboard-routes.js");
+
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+// router.use("/dashboard", dashboardRoutes);
+
+// err route for any resource that doesn't exist
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
